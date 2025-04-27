dbcd709a4fba09e3d6e0ae0ca3d0bad03285d69c
Franklin M <franklinmuriithi431@gmail.com>
2025-04-27 11:13:30 +0300

    ICON ADDED

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+//app/layout.tsx
+//THIS IS THE LAYOUT HOW TO DISPLAY THE ICON
 import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import "./globals.css";
@@ -5,6 +7,7 @@ import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import Footer from "@/components/Home/Footer/Footer";
 import ScrollToTop from "@/components/Helper/ScrollToTop";
 import AnimatedCursor from "react-animated-cursor"
+import Head from 'next/head';
 
 const font = Sora({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
@@ -14,6 +17,9 @@ const font = Sora({
 export const metadata: Metadata = {
   title: "IamFranklin",
   description: "Franklin's Portfolio with Next JS",
+  icons: {
+    icon: "/images/f-icon.png",
+  },
 };
 
 export default function RootLayout({
